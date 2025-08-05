5290b687e98de690deb0b9a712f49dbe606e4b2c
83-TechWorld <sathishkumar.kanda@gmail.com>
2025-08-05 22:33:27 +0530

    init-master

---
+++ employee-workspace-api/src/main/java/com/socgen/crl/nsd/EmployeeWorkspaceApiApplication.java
@@ -0,0 +1,13 @@
+package com.socgen.crl.nsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWorkspaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeWorkspaceApiApplication.class, args);
+	}
+
+}
---
+++ employee-workspace-api/src/test/java/com/socgen/crl/nsd/EmployeeWorkspaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socgen.crl.nsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeWorkspaceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
